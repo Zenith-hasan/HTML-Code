631139f73f99ea3ac5b20ca30754d65b61fc68ce
Zenith_Avro <abuhasan323382@gmail.com>
2025-07-31 22:57:20 +0600

    basic Network discussion

---
+++ 03-Basic/Network.js
@@ -0,0 +1,30 @@
+// HTTP - Hypertext Transfet Protocol
+// Protocol - refers to some specific rules or regulation. Protocols is used to transfer hypertext.
+
+// Stateless Protocols - A stateless protocol is a communication protocol where each request is treated as an independent transaction — completely separate from previous or future requests.
+
+//* Topic to be discussted - 
+        /*  HTTP 
+            Cookies
+            Session
+            Local Storage
+            Tokens
+            Stateless Protocols
+            HTTP Headers
+            Type of Requeest
+            payload
+            HTTP Version
+            TLS 
+            SSL
+            Cache
+            Root Name Server
+            Autoratative Name Server
+            DNS Resoulation */
+
+
+
+
+
+
+
+
